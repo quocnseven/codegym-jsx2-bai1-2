f84be62f1547f8d79891fbce1f39cd3036531a5a
Quoc Nguyen <quocng1997@gmail.com>
2023-07-07 17:55:13 +0700

    them bai tap

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h4>Browser's details: {navigator.userAgent}</h4>
 );
 
 // If you want to start measuring performance in your app, pass a function
